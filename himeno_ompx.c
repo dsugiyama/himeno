dd6faab39a4b8ad96d5781b9ea7704cb58af51ac
Daisuke Sugiyama <dsugiyama@outlook.jp>
2015-02-03 20:56:50 +0900

    implement improved OpenMP version

--- himeno_ompx.c
+++ himeno_ompx.c
@@ -192,40 +192,45 @@ jacobi(int nn)
   int i,j,k,n;
   float gosa, s0, ss;
 
-  for(n=0 ; n<nn ; ++n){
-    gosa = 0.0;
+  #pragma omp parallel
+  {
+    for(n=0 ; n<nn ; ++n){
+      gosa = 0.0;
 
-    for(i=1 ; i<imax-1 ; i++)
-      for(j=1 ; j<jmax-1 ; j++)
-        for(k=1 ; k<kmax-1 ; k++){
-          s0 = a[0][i][j][k] * p[i+1][j  ][k  ]
-             + a[1][i][j][k] * p[i  ][j+1][k  ]
-             + a[2][i][j][k] * p[i  ][j  ][k+1]
-             + b[0][i][j][k] * ( p[i+1][j+1][k  ] - p[i+1][j-1][k  ]
-                              - p[i-1][j+1][k  ] + p[i-1][j-1][k  ] )
-             + b[1][i][j][k] * ( p[i  ][j+1][k+1] - p[i  ][j-1][k+1]
-                               - p[i  ][j+1][k-1] + p[i  ][j-1][k-1] )
-             + b[2][i][j][k] * ( p[i+1][j  ][k+1] - p[i-1][j  ][k+1]
-                               - p[i+1][j  ][k-1] + p[i-1][j  ][k-1] )
-             + c[0][i][j][k] * p[i-1][j  ][k  ]
-             + c[1][i][j][k] * p[i  ][j-1][k  ]
-             + c[2][i][j][k] * p[i  ][j  ][k-1]
-             + wrk1[i][j][k];
+      #pragma omp for reduction(+:gosa) private(i, j, k, s0, ss) collapse(2)
+      for(i=1 ; i<imax-1 ; i++)
+        for(j=1 ; j<jmax-1 ; j++)
+          for(k=1 ; k<kmax-1 ; k++){
+            s0 = a[0][i][j][k] * p[i+1][j  ][k  ]
+               + a[1][i][j][k] * p[i  ][j+1][k  ]
+               + a[2][i][j][k] * p[i  ][j  ][k+1]
+               + b[0][i][j][k] * ( p[i+1][j+1][k  ] - p[i+1][j-1][k  ]
+                                - p[i-1][j+1][k  ] + p[i-1][j-1][k  ] )
+               + b[1][i][j][k] * ( p[i  ][j+1][k+1] - p[i  ][j-1][k+1]
+                                 - p[i  ][j+1][k-1] + p[i  ][j-1][k-1] )
+               + b[2][i][j][k] * ( p[i+1][j  ][k+1] - p[i-1][j  ][k+1]
+                                 - p[i+1][j  ][k-1] + p[i-1][j  ][k-1] )
+               + c[0][i][j][k] * p[i-1][j  ][k  ]
+               + c[1][i][j][k] * p[i  ][j-1][k  ]
+               + c[2][i][j][k] * p[i  ][j  ][k-1]
+               + wrk1[i][j][k];
 
-          ss = ( s0 * a[3][i][j][k] - p[i][j][k] ) * bnd[i][j][k];
+            ss = ( s0 * a[3][i][j][k] - p[i][j][k] ) * bnd[i][j][k];
 
-          gosa+= ss*ss;
-          /* gosa= (gosa > ss*ss) ? a : b; */
+            gosa+= ss*ss;
+            /* gosa= (gosa > ss*ss) ? a : b; */
 
-          wrk2[i][j][k] = p[i][j][k] + omega * ss;
-        }
+            wrk2[i][j][k] = p[i][j][k] + omega * ss;
+          }
 
-    for(i=1 ; i<imax-1 ; ++i)
-      for(j=1 ; j<jmax-1 ; ++j)
-        for(k=1 ; k<kmax-1 ; ++k)
-          p[i][j][k] = wrk2[i][j][k];
-    
-  } /* end n loop */
+      #pragma omp for private(i, j, k) collapse(2)
+      for(i=1 ; i<imax-1 ; ++i)
+        for(j=1 ; j<jmax-1 ; ++j)
+          for(k=1 ; k<kmax-1 ; ++k)
+            p[i][j][k] = wrk2[i][j][k];
+      
+    } /* end n loop */
+  }
 
   return(gosa);
 }
